58f38dc57e76d61439c6ad62860a7d192032110a
Alejandrotos <altove@floridauniversitaria.es>
2024-12-01 20:00:30 +0100

    modificando index y ganado.php para que nos diga que hemos ganado y el botón para volver a index

---
+++ ganado.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>¡Ganaste!</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="resultado">
+        <h1>¡Enhorabuena! Has ganado :)</h1>
+        <a href="index.php" class="boton">Jugar de nuevo</a>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -38,14 +38,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['letra'])) {
 
 // Comprobar si se ha ganado o perdido
 if ($_SESSION['letras_acertadas'] == $_SESSION['palabra']) {
-    echo "¡Enhorabuena! Has ganado :) La palabra era: " . $_SESSION['palabra'] . "<br>";
-    session_destroy();
-    echo '<a href="">Jugar de nuevo</a>';
+    header("Location: ganado.php");
     exit();
 } elseif ($_SESSION['vidas'] <= 0) {
+    header("Location: perdido.php");
     echo "Lo siento, has perdido :( La palabra era: " . $_SESSION['palabra'] . "<br>";
-    session_destroy();
-    echo '<a href="">Jugar de nuevo</a>';
     exit();
 }
 ?>
